31fd8ac23e0079b4b69f7fc4eaeeac5ec8357d30
Pedro Gomes <pedrocgomes@proton.me>
2023-10-09 11:46:56 -0400

    first commit

---
+++ src/main/java/com/pedrogomes/todolist/TodolistApplication.java
@@ -0,0 +1,13 @@
+package com.pedrogomes.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistApplication.class, args);
+	}
+
+}
