613be957949d83181b6938d2193135fa5cf4b1a5
Pedro Gomes <pedrocgomes@proton.me>
2023-10-09 12:17:39 -0400

    create: add User entity

---
+++ src/main/java/com/pedrogomes/todolist/user/UserModel.java
@@ -0,0 +1,40 @@
+package com.pedrogomes.todolist.user;
+
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
